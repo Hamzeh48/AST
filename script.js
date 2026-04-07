36811011446ac2a7e2386ee69638b346590e5def
Hamzeh48 <hamzerifaie@gmail.com>
2026-04-08 00:16:46 +0200

    Refactor functions to use window object on load

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let display = document.getElementById('display');
+window.onload = function() {
+    const display = document.getElementById('display');
 
-function appendValue(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = '';
-}
-
-function calculateResult() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'خطأ';
+    window.appendValue = function(value) {
+        display.value += value;
     }
-}
+
+    window.clearDisplay = function() {
+        display.value = '';
+    }
+
+    window.calculateResult = function() {
+        try {
+            display.value = eval(display.value);
+        } catch (error) {
+            display.value = 'خطأ';
+        }
+    }
+};
